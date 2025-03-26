550c45e6dab927282af23714499c883f5662ca62
evt13 <isaacmerenciodesouza@gmail.com>
2025-03-25 21:43:50 -0300

    Create ex-11.py

---
+++ ex-11.py
@@ -0,0 +1,6 @@
+#11 - Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+print("digite um numero")
+a=int(input())
+antecessor=a-1
+print(antecessor)
