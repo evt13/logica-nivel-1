1f554092d98681100914a0eb867f3198ef89169b
evt13 <isaacmerenciodesouza@gmail.com>
2025-03-25 21:33:42 -0300

    Create ex-9.2.py

---
+++ ex-9.2.py
@@ -0,0 +1,8 @@
+#9.2 - escreva um software que some dois numeros.
+
+print("DIGITE O PRIMEIRO NUMERO")
+a = int(input())
+print("DIGITE O SEGUNDO NUMERO")
+b = int(input())
+soma =a + b
+print(soma)
