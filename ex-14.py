5b18880121a6180adcf6ca5486ca06e4be3684f9
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-08 21:38:17 -0300

    Create ex-14.py

---
+++ ex-14.py
@@ -0,0 +1,9 @@
+14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+TERMINAR 
+print("digite um valor")
+a=int(input())
+if a>10 :
+  print("é maior que 10")
+else a<10 :
+  print("é menor que 10")
