de6c45b6ba834566e6e61ec97d9938b0e544987e
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-15 20:28:08 -0300

    Update ex-14.py

--- ex-14.py
+++ ex-14.py
@@ -1,9 +1,9 @@
 14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
 
-TERMINAR 
 print("digite um valor")
 a=int(input())
-if a>10 :
+if a>10:
   print("é maior que 10")
-else a<10 :
-  print("é menor que 10")
+elif a<10:
+     print("é menor que 10")
+     #Se você quer testar se a é menor que 10, deve usar elif, que permite uma condição. O else é usado sem condição, apenas como "qualquer outro caso".
