7ce08851dd383e602f0e4f3ce6046feb1221e274
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-15 21:36:52 -0300

    Create wx-17.py

---
+++ wx-17.py
@@ -0,0 +1,14 @@
+#Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+
+#etapas para resolver:
+	#1 - peça os 3 valores
+	#2 - descobrir e mostrar o maior
+terminar
+print("Digite um número")
+valor1=int(input())
+print("Digite outro número")
+valor2=int(input())
+print("Digite mais um número")
+valor3=int(input())
+if (valor1>valor2)>valor3):
+    print("o valor1 é o maior")
