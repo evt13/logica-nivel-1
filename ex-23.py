5cef4f4cfedeae82f3f8cb9635da063e0203ad53
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-26 14:31:27 -0300

    Create ex-23.py

---
+++ ex-23.py
@@ -0,0 +1,8 @@
+#23) Contagem regressiva de 10 a 1
+
+n = 10           # Começa com 10
+while n > 0:     # Enquanto n for maior que 0...
+    print(n)     # Mostra o número
+    n -= 1       # Diminui 1 (vai pra 9, depois 8, etc.)
+print("Fogo! 🚀") # Quando sair do while, imprime isso
+
