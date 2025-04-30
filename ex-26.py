cf5aaab3b4ff840d8ab9bf53beefdea1f32d982d
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-29 21:19:26 -0300

    Create ex-26.py

---
+++ ex-26.py
@@ -0,0 +1,14 @@
+#Ler 5 números digitados pelo usuário e armazenar em um vetor. Depois, imprime os números informados
+
+numeros = []
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+d = int(input("Digite o primeiro valor: "))
+e = int(input("Digite o segundo valor: "))
+numeros = [a,b,c,d,e]
+print([a])
+print([b])
+print([c])
+print([d])
+print([e])
