ffec2006c51cb7d5354f56495af666429231384f
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-29 21:21:04 -0300

    Create ex-27.py

---
+++ ex-27.py
@@ -0,0 +1,10 @@
+#Crie um vetor com 5 números e imprima o primeiro valor
+
+numeros = []
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+d = int(input("Digite o primeiro valor: "))
+e = int(input("Digite o segundo valor: "))
+numeros = [a,b,c,d,e]
+print([a])
